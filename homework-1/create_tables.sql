41d8fca196dc214a594985cc14074a05695a034c
alisa <despero45@gmail.com>
2023-07-15 17:44:59 +1000

    Прописана таблица sql с работой по созданию таблиц. Написан скрипт, копирующий данные из файлов cvs с помощью метода copy_from.

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(100),
+	contact_name varchar(100)
+);
+CREATE TABLE employees(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+CREATE TABLE orders(
+	order_id int PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date,
+	ship_city varchar(100)
+);
+
+SELECT * from customers;
+SELECT * from employees;
+SELECT * from orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,39 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='9000'
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+            """Здесь использую очень короткий и оперативный метод"""
+            with open('/home/alisa/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv',
+                      "r") as f:
+                next(f)
+                cur.copy_from(f, 'customers', sep=",")
+                conn.commit()
+            """Здесь пришлось использовать более ёмкий, т.к. в столбце notes используются запятые,
+            не знала, какой сепаратор подабрать"""
+            with open('/home/alisa/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv',
+                      "r") as f:
+                next(f)
+                reader = csv.reader(f)
+                for row in reader:
+                    employee_id, first_name, last_name, title, birth_date, notes = row
+                    query = f"INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) " \
+                            f"VALUES ('{employee_id}', '{first_name}', '{last_name}', '{title}', '{birth_date}', '{notes}')"
+                    cur.execute(query)
+                conn.commit()
+            """Тут опять подошел простой метод"""
+            with open('/home/alisa/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv', "r") as f:
+                next(f)
+                cur.copy_from(f, 'orders', sep=",")
+                conn.commit()
+
+finally:
+    conn.close()
